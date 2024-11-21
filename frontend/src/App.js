bae2bedf478811d6292bc7c342d63ea6e9f4a821
jamesrgoode <85007877+jamesrgoode@users.noreply.github.com>
2024-11-21 17:05:55 -0500

    Started frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,23 @@
+import logo from './logo.svg';
 import './App.css';
-import './components/ResumeUploadForm'
-import ResumeUploadForm from './components/ResumeUploadForm';
+
 function App() {
   return (
-    <div >
-      <ResumeUploadForm/>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
