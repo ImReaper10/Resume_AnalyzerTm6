1060e411e5ea789739fd1d7cc5e3256db76d1422
jamesrgoode <jimmyshimmy1@gmail.com>
2024-11-24 14:46:55 -0500

    Add CSS for login and signup, also made signup route to login

--- frontend/src/App.test.js
+++ frontend/src/App.test.js
@@ -1,7 +1,11 @@
+/* eslint-disable testing-library/no-node-access */
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import { setMocking } from './utils/networkmanager.js';
+import { setMocking, getUploadedData} from './utils/networkmanager.js';
 import userEvent from '@testing-library/user-event'
+import * as fs from 'fs';
+import exp from 'constants';
+import { fail } from 'assert';
 
 class MockLocalStorage {
   constructor() {
@@ -51,5 +55,115 @@ test('Logging in', async () => {
       expect(!!localStorage.getItem("jwt")).toEqual(true)
       res()
     }, 1000);
+  });
+});
+
+test('Uploading valid pdf resume (after trying invalid files)', async () => {
+  render(<App />);
+  const fileInput = screen.getByLabelText("Select Resume (PDF/DOCX):");
+  const jobDescriptionInput = screen.getByLabelText("Job Description:");
+  console.log(fs.readFileSync("../tests/test-files/invalid-file.txt"));
+  userEvent.upload(fileInput, new File([fs.readFileSync("../tests/test-files/invalid-file.txt")], "mypdf.pdf", { type: "text/plain" })); //Meaning a fake pdf
+  userEvent.type(jobDescriptionInput, "Some job description");
+  expect(document.getElementsByClassName("character-count")[0].innerHTML.trim()==="20 characters").toEqual(true);
+  screen.getByText("Invalid file type. Only PDF or DOCX files are allowed.");
+  userEvent.upload(fileInput, new File([fs.readFileSync("../tests/test-files/large-file.pdf")], "mypdf2.pdf", { type: "application/pdf" }));
+  screen.getByText("File size exceeds the limit of 2MB.");
+  userEvent.upload(fileInput, new File([fs.readFileSync("../tests/test-files/large-docx.docx")], "resume.docx", { type: "application/vnd.openxmlformats-officedocument.wordprocessingml.document" }));
+  screen.getByText("File size exceeds the limit of 2MB.");
+  userEvent.upload(fileInput, new File([fs.readFileSync("../tests/test-files/valid-pdf.pdf")], "mypdf3.pdf", { type: "application/pdf" }));
+  try
+  {
+    screen.getByText("File size exceeds the limit of 2MB.");
+    fail("Should not have got a: \"File size exceeds the limit of 2MB.\"");
+  }
+  catch(e) {}
+  try
+  {
+    screen.getByText("Invalid file type. Only PDF or DOCX files are allowed.");
+    fail("Should not have got a: \"Invalid file type. Only PDF or DOCX files are allowed.\"");
+  }
+  catch(e) {}
+  userEvent.click(screen.getByText("Upload"));
+  await new Promise((res, err) => {
+    setTimeout(async () => {
+      let data = await getUploadedData();
+      expect(data.data.resumeText).toEqual("Mock resume text")
+      expect(data.data.jobDescription).toEqual("Some job description")
+      screen.getByText("Resume Fit Score");
+      let viewUploadedData = screen.getByText("View uploaded data");
+      userEvent.click(viewUploadedData);
+      await new Promise((res2) => {
+        setTimeout(async () => {
+          screen.getByText("Mock resume text");
+          screen.getByText("Some job description");
+          res2();
+        }, 1000);
+      });
+      try
+      {
+        let viewKeywords = screen.getByText("Keywords");
+        userEvent.click(viewKeywords);
+        await new Promise((res2) => {
+          setTimeout(async () => {
+            screen.getByText("Matched Skills and Keywords");
+            res2()
+          },500);
+        });
+      }
+      catch(e)
+      {
+        err(e);
+      }
+      try
+      {
+        let viewSuggestions = screen.getByText("Suggestions");
+        userEvent.click(viewSuggestions);
+        await new Promise((res2) => {
+          setTimeout(async () => {
+            screen.getByText("Improvement Suggestions");
+            res2()
+          },500);
+        });
+      }
+      catch(e)
+      {
+        err(e);
+      }
+      res()
+    }, 1000);
+  });
+});
+
+test('Signing out', async () => {
+  render(<App />);
+  await new Promise((res, err) => {
+    setTimeout(async () => {
+      try
+      {
+        userEvent.click(screen.getByText("Sign Out"));
+      }
+      catch(e)
+      {
+        err(e)
+        return;
+      }
+      res();
+    },500);
+  });
+  render(<App />); //Basically refresh the page
+  await new Promise((res, err) => {
+    setTimeout(async () => {
+      try
+      {
+        expect(screen.getAllByText("Login").length).toEqual(2);
+      }
+      catch(e)
+      {
+        err(e)
+        return;
+      }
+      res();
+    },500);
   });
 });
--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,57 +1,81 @@
 import React, { useEffect, useState } from "react";
 import { login , getAccountInfo} from "../utils/networkmanager.js";
 import { useNavigate } from "react-router-dom";
+import "../styling/Login.css";
 
-//BELOW IS JUST A TEST PAGE TO SEE IF THE ABOVE WORKS PROPERLY
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
     const navigate = useNavigate();
+    const [showSignUpSuccess, setSignUpSuccess] = useState(false);
+    const [failMessage, setFailMessage] = useState("");
 
     React.useEffect(() => {
+        if(localStorage.getItem("signup") && localStorage.getItem("signup") === "yes")
+        {
+            localStorage.setItem("signup", "no");
+            setSignUpSuccess(true);
+        }
         getAccountInfo().then((account) => {
             if(account.success)
             {
                 navigate("/upload");
             }
-        });
+        });     
     }, [])
 
     const handleSubmit = async (e) => {
         e.preventDefault();
+        setFailMessage("");
         const result = await login(email, password);
         if (result.success) {
             console.log("Login successful! Token:", result.token);
             localStorage.setItem("jwt", result.token);
             navigate("/upload");
         } else {
+            setFailMessage(result.message);
             console.error("Login failed:", result.message);
         }
     };
 
     return (
-        <div>
+        <div className="form-container">
             <h2>Login</h2>
+            {showSignUpSuccess &&
+                <h5 style={{color: "green"}}>Sign up successful! Please log in with your new credentials.</h5>
+            }
             <form onSubmit={handleSubmit}>
-                <div>
-                    <label>Email:</label>
+                <div className="form-field">
+                    <label style={{marginRight: "10px"}} htmlFor="email">Email:</label>
                     <input
                         type="email"
                         value={email}
                         placeholder="Email"
+			            id="email"
                         onChange={(e) => setEmail(e.target.value)}
                     />
                 </div>
-                <div>
-                    <label>Password:</label>
+                <div className="form-field">
+                    <label style={{marginRight: "10px"}} htmlFor="password">Password:</label>
                     <input
+                        id="password"
                         type="password"
                         value={password}
                         placeholder="Password"
                         onChange={(e) => setPassword(e.target.value)}
                     />
                 </div>
-                <button type="submit">Login</button>
+                {failMessage &&
+                    <div style={{marginTop: "5px", marginBottom: "10px"}}>
+                        <em style={{color: "red"}}>{failMessage}</em>
+                    </div>
+                }
+                <button type="submit" style={{marginRight: "10px"}} disabled={email.length === 0 || password.length === 0}>
+                    Login
+                </button>
+                <button onClick={() => {navigate("/signup")}} type="button">
+                    Sign Up
+                </button>
             </form>
         </div>
     );
