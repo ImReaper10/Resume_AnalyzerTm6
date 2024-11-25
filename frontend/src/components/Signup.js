ea7a877846a70b91d2ec00bb9889183757a13733
DiegoVelasquezMinaya <157928640+DiegoVelasquezMinaya@users.noreply.github.com>
2024-11-25 15:52:35 -0500

    Update Signup.js

--- frontend/src/components/Signup.js
+++ frontend/src/components/Signup.js
@@ -34,6 +34,10 @@ const Signup = () => {
 
     return (
         <div className="form-container">
+            <link
+                rel="stylesheet"
+                href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900&amp;display=swap"
+            />
             <h2>Sign Up</h2>
             <form onSubmit={handleSubmit}>
                 <div className="form-field">
